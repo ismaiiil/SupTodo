93c6328aa07f49fac7e5de33ecc67aee009ced02
ismaiiil <ismaiiil@live.com>
2019-01-05 22:41:58 +0400

    + tested register + tested logout + tested read + tested update + tested share

--- app/src/main/java/com/supinfo/and/suptodo/API/UserService.java
+++ app/src/main/java/com/supinfo/and/suptodo/API/UserService.java
@@ -1,6 +1,7 @@
 package com.supinfo.and.suptodo.API;
 
 import com.google.gson.JsonObject;
+import com.supinfo.and.suptodo.model.StateResponse;
 import com.supinfo.and.suptodo.model.TodoResponse;
 
 import java.util.List;
@@ -11,9 +12,30 @@ import retrofit2.http.Query;
 
 public interface UserService {
 
+    @GET("?action=register")
+    Call<JsonObject> register(@Query("username") String username, @Query("password") String password
+            , @Query("firstname") String firstname, @Query("lastname") String lastname
+            , @Query("email") String email);
+
     @GET("?action=login")
     Call<JsonObject> login(@Query("username") String username, @Query("password") String password);
 
+    @GET("?action=logout")
+    Call<StateResponse> logout();
+
     @GET("?action=list")
     Call<List<TodoResponse>> list(@Query("username") String username, @Query("password") String password);
+
+    @GET("?action=read")
+    Call<JsonObject> read(@Query("username") String username, @Query("password") String password
+            , @Query("id") int id);
+
+    @GET("?action=update")
+    Call<JsonObject> update(@Query("username") String username, @Query("password") String password
+            , @Query("id") int id , @Query("todo") String todo);
+
+    @GET("?action=share")
+    Call<JsonObject> share(@Query("username") String username, @Query("password") String password
+            , @Query("user") String userFriend);
+
 }
--- app/src/main/java/com/supinfo/and/suptodo/RegisterActivity.java
+++ app/src/main/java/com/supinfo/and/suptodo/RegisterActivity.java
@@ -20,6 +20,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
+
     UserService userService;
 
     @Override
@@ -27,27 +28,128 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         userService = ApiUtil.getUserService();
+        testRegister();
         testLogin();
         testList();
+        testRead();
+        testUpdate();
+        testRead();
+        testShare();
+        testLogout();
 
     }
 
-    private void testLogin(){
-        Call<JsonObject> call = userService.login("","");
-        System.out.println("calling api");
+    private void testShare() {
+        Call<JsonObject> call = userService.share("userone","test","usertwo");
+        System.out.println("calling api from testShare");
         call.enqueue(new Callback<JsonObject>() {
             Gson gson = new Gson();
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                if (response.body().has("id")){
-                    UserResponse userResponse = gson.fromJson(response.body(),UserResponse.class);
-                    System.out.println(userResponse.getFirstname());
-                } else if (response.body().has("message")){
+                System.out.println("finished calling API from testShare");
+                if (response.body().has("message")){
                     MessageResponse messageResponse = gson.fromJson(response.body(),MessageResponse.class);
-                    System.out.println(messageResponse.getMessage());
+                    System.out.println("the todo was not shared: " + messageResponse.getMessage());
                 }else{
                     StateResponse stateResponse = gson.fromJson(response.body(),StateResponse.class);
-                    System.out.println(stateResponse.isSuccess());
+                    if (stateResponse.isSuccess()) {
+                        System.out.println("the todo was shared: " +stateResponse.isSuccess());
+                    }else{
+                        System.out.println("server rejected this action " +stateResponse.isSuccess());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                System.out.println(t);
+                System.out.println("Something went wrong when trying to connect to the server");
+            }
+
+        });
+    }
+
+    private void testUpdate() {
+        Call<JsonObject> call = userService.update("userone","test",572,"the first todo \n the 2nd todo");
+        System.out.println("calling api from testUpdate");
+        call.enqueue(new Callback<JsonObject>() {
+            Gson gson = new Gson();
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                System.out.println("finished calling API from testUpdate");
+                if (response.body().has("message")){
+                    MessageResponse messageResponse = gson.fromJson(response.body(),MessageResponse.class);
+                    System.out.println("the todo was not updated: " + messageResponse.getMessage());
+                }else{
+                    StateResponse stateResponse = gson.fromJson(response.body(),StateResponse.class);
+                    if (stateResponse.isSuccess()) {
+                        System.out.println("the todo was updated: " +stateResponse.isSuccess());
+                    }else{
+                        System.out.println("server rejected this action " +stateResponse.isSuccess());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                System.out.println(t);
+                System.out.println("Something went wrong when trying to connect to the server");
+            }
+
+        });
+    }
+
+    private void testRead() {
+        Call<JsonObject> call = userService.read("userone","test",572);
+        System.out.println("calling api from testRead");
+        call.enqueue(new Callback<JsonObject>() {
+            Gson gson = new Gson();
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                System.out.println("finished calling API from testRead");
+                if (response.body().has("id")){
+                    TodoResponse todoResponse = gson.fromJson(response.body(),TodoResponse.class);
+                    System.out.println("the todo is with id " + todoResponse.getId() + " and text " + todoResponse.getTodo());
+                } else if (response.body().has("message")){
+                    MessageResponse messageResponse = gson.fromJson(response.body(),MessageResponse.class);
+                    System.out.println("the todo was not read: " + messageResponse.getMessage());
+                }else{
+                    StateResponse stateResponse = gson.fromJson(response.body(),StateResponse.class);
+                    System.out.println("server rejected this action " +stateResponse.isSuccess());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                System.out.println(t);
+                System.out.println("Something went wrong when trying to connect to the server");
+            }
+
+        });
+
+
+    }
+
+    private void testLogin(){
+        Call<JsonObject> call = userService.login("userone","test");
+        System.out.println("calling api from testLogin");
+        call.enqueue(new Callback<JsonObject>() {
+            Gson gson = new Gson();
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                System.out.println("finished calling API from testLogin");
+                if (response.body().has("id")){
+                    UserResponse userResponse = gson.fromJson(response.body(),UserResponse.class);
+                    System.out.println("the user has name: " + userResponse.getFirstname() + " " + userResponse.getLastname());
+                } else if (response.body().has("message")){
+                    MessageResponse messageResponse = gson.fromJson(response.body(),MessageResponse.class);
+                    System.out.println("login failed: " + messageResponse.getMessage());
+                }else{
+                    StateResponse stateResponse = gson.fromJson(response.body(),StateResponse.class);
+                    System.out.println("server rejected this action " + stateResponse.isSuccess());
                 }
 
             }
@@ -62,25 +164,76 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
+    private void testLogout(){
+        Call<StateResponse> call = userService.logout();
+        System.out.println("calling api from testLogout");
+        call.enqueue(new Callback<StateResponse>() {
+            @Override
+            public void onResponse(Call<StateResponse> call, Response<StateResponse> response) {
+                System.out.println("finished calling API from testLogout");
+                System.out.println("You were sucessfully logged out: " + response.body().isSuccess());
+            }
+
+            @Override
+            public void onFailure(Call<StateResponse> call, Throwable t) {
+                System.out.println(t);
+                System.out.println("Something went wrong when trying to connect to the server");
+            }
+        });
+    }
+
+    private void testRegister(){
+        Call<JsonObject> call = userService.register("userten","test","jeff",
+                "maname","test@mail.com");
+        System.out.println("calling api from testRegister");
+        call.enqueue(new Callback<JsonObject>() {
+            Gson gson = new Gson();
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                System.out.println("finished calling API from testRegister");
+                if (response.body().has("id")){
+                    UserResponse userResponse = gson.fromJson(response.body(),UserResponse.class);
+                    System.out.println("the user has name: " + userResponse.getFirstname() + " " + userResponse.getLastname() + " was registered and logged in");
+                } else if (response.body().has("message")){
+                    MessageResponse messageResponse = gson.fromJson(response.body(),MessageResponse.class);
+                    System.out.println("You were not registered since: " + messageResponse.getMessage());
+                }else{
+                    StateResponse stateResponse = gson.fromJson(response.body(),StateResponse.class);
+                    System.out.println("You were not registered: "+stateResponse.isSuccess());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                System.out.println(t);
+                System.out.println("Something went wrong when trying to connect to the server");
+            }
+        });
+    }
+
     private void testList(){
         Call<List<TodoResponse>> call = userService.list("userone","test");
-        System.out.println("calling api");
+        System.out.println("calling api from testList");
         call.enqueue(new Callback<List<TodoResponse>>() {
             @Override
             public void onResponse(Call<List<TodoResponse>> call, Response<List<TodoResponse>> response) {
+                System.out.println("finished calling API from testList");
                 for(TodoResponse todoResponse:response.body()){
-                    System.out.println(todoResponse.getId() + " " + todoResponse.getLastupdate());
+                    System.out.print(todoResponse.getId() + " " + todoResponse.getLastupdate() + " " + todoResponse.getTodo() + ", ");
                 }
-                System.out.println("finised calling api");
+                System.out.println();
+
             }
 
             @Override
             public void onFailure(Call<List<TodoResponse>> call, Throwable t) {
                 System.out.println(t);
-                System.out.println("finised calling api");
+                System.out.println("Something went wrong when trying to connect to the server");
             }
 
         });
 
     }
+
+
 }
