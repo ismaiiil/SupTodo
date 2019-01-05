1af7e77004b923f345dfdefdb38144426a905fa7
ismaiiil <ismaiiil@live.com>
2019-01-05 17:09:28 +0400

    + added MessageResponse model + added StateResponse model

---
+++ app/src/main/java/com/supinfo/and/suptodo/model/StateResponse.java
@@ -0,0 +1,13 @@
+package com.supinfo.and.suptodo.model;
+
+public class StateResponse {
+    private boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
