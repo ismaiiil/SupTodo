5a899c5839b9eed9742dbc5280b07bc93e3b2bf5
ismaiiil <ismaiiil@live.com>
2019-01-07 12:03:33 +0400

    + made the todo list screen conform to assignment:
    + In a menu, burger or flotting button, the current user can link the todolist with an another
    user.
    + A click on a todolist display it in a new activity
    + logout put in the menu burger on top

--- app/src/main/java/com/supinfo/and/suptodo/ToDoListActivity.java
+++ app/src/main/java/com/supinfo/and/suptodo/ToDoListActivity.java
@@ -1,8 +1,15 @@
 package com.supinfo.and.suptodo;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -10,41 +17,66 @@ import android.widget.Button;
 
 public class ToDoListActivity extends AppCompatActivity {
 
-    private Button btnLogout;
-    private Button btnAddToDoList;
-    private Button btnEditToDoList;
+//    private Button btnLogout;
+//    private Button btnAddToDoList;
+//    private Button btnEditToDoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
-
-        getUIElements();
-
-        btnLogout.setOnClickListener(v ->
-                exitApplication()
-
-        );
-
-        btnAddToDoList.setOnClickListener(v ->
-                openAddActivity()
-        );
-
-        btnEditToDoList.setOnClickListener(v ->
-                openEditActivity()
-        );
+        this.setTheme(R.style.AppTheme_NoActionBar);
 
         String[] todo = {"machin1 \n machin0.5", "machin2", "machin3", "machin4", "machin5", "machin6", "machin7", "machin8", "machin9", "machin10"};
         ListAdapter multiListViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todo);
         ListView multiListView = (ListView)findViewById(R.id.multiListView);
         multiListView.setAdapter(multiListViewAdapter);
+
+
+        multiListView.setOnItemClickListener((parent, view, position, id) -> {
+            openEditActivity();
+        });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle("Your todo lists:");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            openAddActivity();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_to_do_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            System.out.println("logout pressed");
+            exitApplication();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void getUIElements(){
 
-        btnLogout = (Button)findViewById(R.id.logoutBtn);
-        btnAddToDoList = (Button)findViewById(R.id.addBtn);
-        btnEditToDoList = (Button)findViewById(R.id.editBtn);
+//        btnLogout = (Button)findViewById(R.id.logoutBtn);
+//        btnAddToDoList = (Button)findViewById(R.id.addBtn);
+//        btnEditToDoList = (Button)findViewById(R.id.editBtn);
     }
 
     public void exitApplication(){
